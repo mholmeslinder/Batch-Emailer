25b19bfe962f84ac3335531e9834d0b73c7e5e4c
Matt Linder <mholmeslinder@gmail.com>
2020-01-23 12:39:29 -0800

    Initial skeleton/pseudocode

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+from keys import key
+
+domain = #'mobiustrio.org' ??
+recipients = []
+codes =  []
+pairs = {}
+
+request_url = f'https://api.mailgun.net/v2/{domain}/messages'
+# TODO open recipients csv and use for loop to turn emails into recipients
+
+# TODO open bandcamp codes csv and and use for loop to turn codes into codes
+
+# TODO  zip those into a dict?
+
+''' TODO use for loop to iterate through lists or dict, 
+making request for each recipient/code pair and inserting them into 
+{recipient} and {code} '''
+
+for recipient, code in pairs:
+    request = requests.post(request_url, auth=('api', key), data={
+        'from': 'KickstarterFulfillment@mobiustrio.org',
+        'to': {recipient},
+        'subject': 'A digital download of Bon Voyage, for you!' ,
+        'text': f"Hello, wonderful Kickstarter backer! \
+            You wanted a digital download of our new album Bon Voyage,\
+            so here is a code to download it from Bandcamp. {code}\
+            The way it works is: you navigate to \
+            http://mobiustrio.bandcamp.com/yum , and then you enter \
+            the code! It's as simple as that! We hope you love it.\
+            Love, The Guys at Mobius Trio"
+    })
+
+print(f'Status: {request.status_code}')
+print(f'Body:   {request.text}')
