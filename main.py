a9cb4a6a3fb4d4d1fafb9dc4573a6f8ed6f3d83d
Matt Linder <mholmeslinder@gmail.com>
2020-01-24 18:56:47 -0800

    started importing csvs in main

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 # Mobius Trio Kickstarter 2019 Bandcamp code sender
 import requests
+import pandas as pd
 # keeping API key and domain secret and safe in their own lil modules
 from keys import key, domain
 
-recipients = []
-codes =  []
-pairs = {}
+# open backers csv and use for loop to turn emails into recipients
+df1 = pd.read_csv('backers.csv', usecols=[3])
+recipients = list(df1)
 
-request_url = f'https://api.mailgun.net/v2/{domain}/messages'
-# TODO open recipients csv and use for loop to turn emails into recipients
+# open bandcamp codes csv and and use for loop to turn codes into codes
+df2 = pd.read_csv('codes.csv', usecols=[0])
+codes =  list(df2)
 
-# TODO open bandcamp codes csv and and use for loop to turn codes into codes
-
-# TODO  zip those into a dict
+# zip those into a dict
+pairs = dict(zip(recipients, codes))
 
 ''' TODO use for loop to iterate through dict, 
 making request for each recipient/code pair and inserting them into 
 {recipient} and {code} '''
+
+request_url = f'https://api.mailgun.net/v2/{domain}/messages'
 
 for recipient, code in pairs:
     request = requests.post(request_url, auth=('api', key), data={
@@ -28,8 +31,8 @@ for recipient, code in pairs:
             so here is a code to download it from Bandcamp: {code}\n\n\
             The way it works is: you navigate to \
             http://mobiustrio.bandcamp.com/yum , and then you enter \
-            the code! It's as simple as that! We hope you love it.\n\
-            Love, The Guys at Mobius Trio"
+            the code! It's as simple as that. We hope you love it.\n\
+            Love,\n The Guys at Mobius Trio"
     })
 
 print(f'Status: {request.status_code}')
