960ca1cfe8d684bca0076deff34399f385256caa
Matt Linder <mholmeslinder@gmail.com>
2020-01-29 09:31:05 -0800

    created logfile, added error handling, etc.

--- main.py
+++ main.py
@@ -1,35 +1,47 @@
 # Mobius Trio Kickstarter 2019 Bandcamp code sender
+# TODO Implement OOP version
 import requests
 import csv
-# # TODO Take API key and csv file as environment variables
+from sys import argv
+
+# Take API key and csv file as arguments
+script, codes_file, key = argv
 
 # open csv and put recipients and codes into lists
 pairs = {}
-with open('./ignore/mailgun.csv') as csvfile:
+with open(codes_file) as csvfile:
     read_csv = csv.reader(csvfile, delimiter=',')
     for row in read_csv:
         pairs[row[1]] = row[0]
 
-print(pairs)
-''' use for loop to iterate through dict, 
-making request for each recipient/code pair and inserting them into 
-{recipient} and {code} '''
+# TODO how to not hardcode domain and body info?
+domain = 'sandbox22a8ba83fc5c4a82ac0f14cb008c531f.mailgun.org'
+request_url = f'https://api.mailgun.net/v2/{domain}/messages'
 
-# request_url = f'https://api.mailgun.net/v2/{domain}/messages'
+# open file for recording recipients, codes, status, and msg body
+log = open("./ignore/log_file.txt", "a+")
+# Take pairs dict and send email to recipient with code in body
+for recipient, code in pairs.items():
+    try:
+        body = f"""
+Hello, wonderful Kickstarter backer! You wanted a digital download of our new album Bon Voyage,
+so here is a code to download it from Bandcamp: {code}\n\n
+The way it works is: you navigate to http://mobiustrio.bandcamp.com/yum , and then you enter the code! 
+It's as simple as that. We hope you love it.
 
-# for recipient, code in pairs.items():
-#     request = requests.post(request_url, auth=('api', key), data={
-#         'from': 'KickstarterFulfillment@mobiustrio.org',
-#         'to': {recipient},
-#         'subject': 'A digital download of Bon Voyage, for you!' ,
-#         'text': f"Hello, wonderful Kickstarter backer! \
-#             You wanted a digital download of our new album Bon Voyage,\
-#             so here is a code to download it from Bandcamp: {code}\n\n\
-#             The way it works is: you navigate to \
-#             http://mobiustrio.bandcamp.com/yum , and then you enter \
-#             the code! It's as simple as that. We hope you love it.\n\
-#             Love,\n The Guys at Mobius Trio"
-#     })
-
-# print(f'Status: {request.status_code}')
-# print(f'Body:   {request.text}')
+Love,
+The Guys at Mobius Trio
+"""
+        # TODO Make this dict, esp body text, more elegant/customizable
+        request = requests.post(request_url, auth=('api', key), data={
+            'from': 'KickstarterFulfillment@mobiustrio.org',
+            'to': recipient,
+            'subject': 'A digital download of Bon Voyage, for you!' ,
+            'text': body
+})
+        # write recipient, code, status, and msg body to file
+        log.write(f"Recipient: {recipient} {body} Status code: {request.status_code}\n\n")
+    except requests.exceptions.RequestException as e:
+        # print exception to same file as everything else
+        log.write(f"Had error: {e} when emailing {recipient}")
+        continue
