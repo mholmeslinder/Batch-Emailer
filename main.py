f1d420e4f1c1db1b3b5a0920ff1af8b1d4dce3c4
Matt Linder <mholmeslinder@gmail.com>
2020-02-04 09:36:24 -0800

    functioning OOP version. next = non-hardcoded msg body

--- main.py
+++ main.py
@@ -3,12 +3,8 @@ import requests
 import csv
 from sys import argv
 
-# Take API key and csv file as arguments
-# _, domain, codes_file, key = argv
-# domain = 'mail.mobiustrio.org'
 
-# TODO create Loader
-class Loader(object):
+class Message(object):
 
     def __init__(self):
         # Take API key and csv file as arguments
@@ -19,29 +15,21 @@ class Loader(object):
             read_csv = csv.reader(csvfile, delimiter=',')
             for row in read_csv:
                 self.pairs[row[1]] = row[0]
+        self.subject = "A Bandcamp download code for Bon Voyage"
 
 
-
-# TODO create Sender
 class Sender(object):
 
     def __init__(self):
-    # open file for recording recipients, codes, status, and msg body.
-    # File is created if it doesn't exist. Append only.
-        self.log = open("./ignore/log_file.txt", "a+")
-        self.subject = "A Bandcamp download code for Bon Voyage"
+        pass
         
-    def send_batch_message(self, Loader):
+    def send_batch_message(self, Message):
+        # open file for recording recipients, codes, status, and msg body.
+        # File is created if it doesn't exist. Append only.
+        log = open("./ignore/log_file.txt", "a+")
         # Take pairs dict and send email to recipient with code in body
-        for recipient, code in Loader.pairs.items():
-            try:
-                return requests.post(
-                f"https://api.mailgun.net/v3/{Loader.domain}/messages",
-                auth=("api", Loader.key),
-                data={"from": f"mail@{Loader.domain}>",
-                    "to": [recipient],
-                    "subject": self.subject,
-                    "text": f"""
+        for recipient, code in Message.pairs.items():
+            body = f"""
 Hello, wonderful Kickstarter backer! You wanted a digital download of our new album Bon Voyage,
 so here is a code to download it from Bandcamp: {code}\n\n
 The way it works is: you navigate to http://mobiustrio.bandcamp.com/yum , and then you enter the code! 
@@ -49,16 +37,24 @@ It's as simple as that. We hope you love it.
 
 Love,
 The Guys at Mobius Trio
-"""})
+""" 
+            try:
+                requests.post(
+                f"https://api.mailgun.net/v3/{Message.domain}/messages",
+                auth=("api", Message.key),
+                data={"from": f"mail@{Message.domain}",
+                    "to": recipient,
+                    "subject": Message.subject,
+                    "text": body})
                 # write recipient, code, status, and msg body to file
-                self.log.write(f"Recipient: {recipient} {requests.status}\n\n")
+                log.write(f"Recipient: {recipient} \n\n")
             except requests.exceptions.RequestException as e:
-                # print exception to same file as everything else
-                self.log.write(f"Had error: {e} when emailing {recipient}")
+                # write exception to same file as everything else
+                log.write(f"Had error: {e} when emailing {recipient}")
                 continue
         # close log
-        self.log.close()
+        log.close()
 
-msg_data = Loader()
+msg_data = Message()
 msg_send = Sender()
 msg_send.send_batch_message(msg_data)
