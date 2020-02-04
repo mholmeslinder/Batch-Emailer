b0e7e53a0f86b091850d1f67ba8c7221f15dad5e
Matt Linder <mholmeslinder@gmail.com>
2020-02-04 08:57:24 -0800

    first stab at OOP version

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
 # Mobius Trio Kickstarter 2019 Bandcamp code sender
-# TODO Implement OOP version
 import requests
 import csv
 from sys import argv
 
 # Take API key and csv file as arguments
-script, codes_file, key = argv
+# _, domain, codes_file, key = argv
+# domain = 'mail.mobiustrio.org'
 
-# open csv and put recipients and codes into lists
-pairs = {}
-with open(codes_file) as csvfile:
-    read_csv = csv.reader(csvfile, delimiter=',')
-    for row in read_csv:
-        pairs[row[1]] = row[0]
+# TODO create Loader
+class Loader(object):
 
-# TODO how to not hardcode domain and body info?
-domain = 'sandbox22a8ba83fc5c4a82ac0f14cb008c531f.mailgun.org'
-request_url = f'https://api.mailgun.net/v2/{domain}/messages'
+    def __init__(self):
+        # Take API key and csv file as arguments
+        self._, self.domain, self.codes_file, self.key = argv
+        # open csv and put recipients and codes into lists
+        self.pairs = {}
+        with open(self.codes_file) as csvfile:
+            read_csv = csv.reader(csvfile, delimiter=',')
+            for row in read_csv:
+                self.pairs[row[1]] = row[0]
 
-# open file for recording recipients, codes, status, and msg body
-log = open("./ignore/log_file.txt", "a+")
-# Take pairs dict and send email to recipient with code in body
-for recipient, code in pairs.items():
-    try:
-        body = f"""
+
+
+# TODO create Sender
+class Sender(object):
+
+    def __init__(self):
+    # open file for recording recipients, codes, status, and msg body.
+    # File is created if it doesn't exist. Append only.
+        self.log = open("./ignore/log_file.txt", "a+")
+        self.subject = "A Bandcamp download code for Bon Voyage"
+        
+    def send_batch_message(self, Loader):
+        # Take pairs dict and send email to recipient with code in body
+        for recipient, code in Loader.pairs.items():
+            try:
+                return requests.post(
+                f"https://api.mailgun.net/v3/{Loader.domain}/messages",
+                auth=("api", Loader.key),
+                data={"from": f"mail@{Loader.domain}>",
+                    "to": [recipient],
+                    "subject": self.subject,
+                    "text": f"""
 Hello, wonderful Kickstarter backer! You wanted a digital download of our new album Bon Voyage,
 so here is a code to download it from Bandcamp: {code}\n\n
 The way it works is: you navigate to http://mobiustrio.bandcamp.com/yum , and then you enter the code! 
@@ -31,17 +49,16 @@ It's as simple as that. We hope you love it.
 
 Love,
 The Guys at Mobius Trio
-"""
-        # TODO Make this dict, esp body text, more elegant/customizable
-        request = requests.post(request_url, auth=('api', key), data={
-            'from': 'KickstarterFulfillment@mobiustrio.org',
-            'to': recipient,
-            'subject': 'A digital download of Bon Voyage, for you!' ,
-            'text': body
-})
-        # write recipient, code, status, and msg body to file
-        log.write(f"Recipient: {recipient} {body} Status code: {request.status_code}\n\n")
-    except requests.exceptions.RequestException as e:
-        # print exception to same file as everything else
-        log.write(f"Had error: {e} when emailing {recipient}")
-        continue
+"""})
+                # write recipient, code, status, and msg body to file
+                self.log.write(f"Recipient: {recipient} {requests.status}\n\n")
+            except requests.exceptions.RequestException as e:
+                # print exception to same file as everything else
+                self.log.write(f"Had error: {e} when emailing {recipient}")
+                continue
+        # close log
+        self.log.close()
+
+msg_data = Loader()
+msg_send = Sender()
+msg_send.send_batch_message(msg_data)
