b8926105dd150fceedc7d02bb6d377460aea9dd9
Matt Linder <mholmeslinder@gmail.com>
2020-02-05 21:13:52 -0800

    created modulized version

---
+++ ks_script.py
@@ -0,0 +1,34 @@
+# ks_script.py
+from sys import argv
+import csv
+import mailer
+
+# since these are a little more private we pull them in from argv, 
+# though if message body was sensitive enough that we didn't want to check in on github \
+# then you might not want to include that in the file as well
+# import codes_file
+_, api_key, domain, codes_file, log_file_path = argv
+sender = mailer.Sender(api_key=api_key, domain=domain, log_file_path=log_file_path)
+
+# open csv, create download code:recipient mapping inside this script
+pairs = {}
+with open(codes_file) as csvfile:
+    read_csv = csv.reader(csvfile, delimiter=',')
+    for row in read_csv:
+        pairs[row[1]] = row[0] 
+
+messages = []
+for recipient, code in pairs.items():
+    messages.append(mailer.Message(
+        subject='cool subject',
+        from_address='no-reply@thing.com',
+        to=recipient,
+        body=f'Here is your damn {code}',)
+    )
+
+# can do this, or expose a sender.Load(messages) and sender.SendLoaded().
+#  Or do a sender.SendBulk(messages).
+for message in messages:
+    sender.send(message)
+
+print(f'Script completed! Logs written to {log_file_path}')
---
+++ mailer.py
@@ -0,0 +1,40 @@
+# mailer module for ks_script
+import requests
+
+
+class Message(object):
+
+    def __init__(self, subject, from_address, to, body):
+        self.subject = subject 
+        self.from_address = from_address
+        self.to = to
+        self.body = body
+
+
+class Sender(object):
+
+    def __init__(self, api_key, domain, log_file_path):
+        self.api_key = api_key
+        self.domain = domain
+        self.log_file_path = log_file_path
+        
+    def send(self, Message):
+        # open file for recording recipients, codes, status, and msg body.
+        # File is created if it doesn't exist. Append only.
+        log = open(self.log_file_path, "a+")
+        try:
+            requests.post(
+            f"https://api.mailgun.net/v3/{self.domain}/messages",
+            auth=("api", self.api_key),
+            data={"from": Message.from_address,
+                "to": Message.to,
+                "subject": Message.subject,
+                "text": Message.body})
+            # write recipient, code, status, and msg body to file
+            log.write(f"Recipient: {Message.to} \n\n")
+        except requests.exceptions.RequestException as e:
+            # write exception to same file as everything else
+            log.write(f"Had error: {e} when emailing {Message.to}")
+
+        # close log
+        log.close()
